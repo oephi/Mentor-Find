d83ef75d72285f08d23e3df1159c29c710ecf4f1
Adam Demirel <adxm@msn.com>
2018-10-30 15:51:10 +1100

    Add template link to user cards
    Co-authored-by: Mike Mccallum <mikemgmcc@gmail.com>
    Co-authored-by: Ryan Sickle <r.sickle@protonmail.com>
    Co-authored-by: Sinan Canselen <scanselen@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :feeds
   devise_for :users
 
+  get '/profile', to: 'feeds#show', as: 'profile'
+
   root to: "feeds#home" # For Devise to work, ensure you have defined root_url to *something*.
 end
