6b2f879ebbfc66263ca1f1561c1638d5deda6752
Mike McCallum <mikemgmcc@gmail.com>
2018-11-01 14:48:03 +1100

    showing interests in profile page

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,4 +3,8 @@ class FeedsController < ApplicationController
         @sample = Service.take(100) #TODO
         @interests = User.find(current_user.id).skills 
     end
+
+    def show
+        @interests = current_user.interests
+    end
 end
