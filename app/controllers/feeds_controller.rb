1a285eaf3a44cf43ad3c05f66ab37d0c3ee0abf0
Adam Demirel <adxm@msn.com>
2018-11-01 16:20:28 +1100

    Add param filter functionality for interests

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,19 +1,17 @@
 class FeedsController < ApplicationController
     def home
-        @sample = Service.take(100) #TODO
-        @interests = User.find(current_user.id).skills 
+        #@sample = Service.take(100) #TODO
+        @interests = User.find(current_user.id).skills # for the interests sidebar
+        @category = params[:interest] # for the feeds h1 #TODO: Add an if/then condition for :interest/:search
 
-        # if params[:tag] #with tags
-        #     @links = Tag.find_by(name: params[:tag]).links.where(user_id: current_user.id, read: nil)
-        #     @read_links = Tag.find_by(name: params[:tag]).links.where(user_id: current_user.id, read: "true")
-        # elsif params[:search]  #render json: params  #with tags
-        #     @links = Link.fuzzy_search(params[:search][:term]).where(user_id: current_user.id, read: nil)
-        #     @read_links = Link.fuzzy_search(params[:search][:term]).where(user_id: current_user.id, read: "true")
-        # else #no tags
-        #     @links = Link.where(user_id: current_user.id, read: nil)
-        #     @read_links = Link.where(user_id: current_user.id, read: "true")
-        # end
+        if params[:interest] # run this when interest parameter is present:
+            @sample = Skill.find_by(name: params[:interest]).interests.where(user_id: current_user.id)
+        elsif params[:search] # run this when the search parameter is present:
+            @links = Link.fuzzy_search(params[:search][:term]).where(user_id: current_user.id, read: nil)
+        else # otherwise run this if there's no parameters:
+            @sample = Service.take(100)
+        end
+
+        #TODO: add the `all` tag logic
     end
-
-
 end
