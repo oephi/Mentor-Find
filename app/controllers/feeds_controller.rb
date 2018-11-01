0d237d9b40d4d15b35e9d54b437bb1971dde5713
Adam Demirel <adxm@msn.com>
2018-11-01 14:19:56 +1100

    Send params with sidebar interest links

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,5 +2,18 @@ class FeedsController < ApplicationController
     def home
         @sample = Service.take(100) #TODO
         @interests = User.find(current_user.id).skills 
+
+        # if params[:tag] #with tags
+        #     @links = Tag.find_by(name: params[:tag]).links.where(user_id: current_user.id, read: nil)
+        #     @read_links = Tag.find_by(name: params[:tag]).links.where(user_id: current_user.id, read: "true")
+        # elsif params[:search]  #render json: params  #with tags
+        #     @links = Link.fuzzy_search(params[:search][:term]).where(user_id: current_user.id, read: nil)
+        #     @read_links = Link.fuzzy_search(params[:search][:term]).where(user_id: current_user.id, read: "true")
+        # else #no tags
+        #     @links = Link.where(user_id: current_user.id, read: nil)
+        #     @read_links = Link.where(user_id: current_user.id, read: "true")
+        # end
     end
+
+
 end
