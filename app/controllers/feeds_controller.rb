b917d94178f21db467091fad92c628904e9582b8
Adam Demirel <adxm@msn.com>
2018-11-02 13:06:48 +1100

    Add fallback page for failed search, and update flash message

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,40 +6,28 @@ class FeedsController < ApplicationController
             @sample = Service.all.sample(99) # old: order("RANDOM()").take(99)
             @category = "Mentors in Everything Near You"
             @class = 'active'
-
         
         elsif params[:interest] == "all"
-            #@sample = Interest.where(user_id: current_user.id)
-            #@sample = Service.where(skill_id: Interest.where(user_id: current_user.id))
-            #@sample = Interest.where(user_id: current_user.id).each { |interest| 
-            #           Skill.find(interest.skill_id).services }
-
-
             @category = "Your Mentors Near You"
             interests = User.find(current_user.id).skills.pluck(:id)
-            @sample = Service.where(skill_id:interests)
-
+            @sample = Service.where(skill_id:interests).sample(99)
 
         elsif params[:interest] # run this when interest parameter is present:
-            #@sample = Skill.find_by(name: params[:interest]).interests.where(user_id: current_user.id)
             @sample = Service.where(skill_id: Skill.find_by(name: params[:interest]).id)
             @category = params[:interest] # for the feeds h1
 
-
-
         elsif params[:search] # run this when the search parameter is present:
-            # @search = Skill.fuzzy_search(name: params[:search])
-            #@sample = Service.where(skill_id:  )
-            #@sample = Service.where(skill_id: Skill.find_by(name: params[:interest]).id)
-
             @category = "#{params[:search]} Mentors"
+            @search = Skill.fuzzy_search(params[:search])
 
-            @sample = Service.where(skill_id: Skill.fuzzy_search(params[:search])[0].id)
+            if @search.empty?
+                @sample = Service.take(99)
+                flash.now[:error] = "Sorry there are no mentors for that category yet."
+            else
+                @sample = Service.where(skill_id: @search[0].id)
+            end
 
-            # if @sample.nil?
-            #     @sample = 
-
-
+            # byebug
 
         else # otherwise run this if there's no parameters:
             @sample = Service.take(99) #TODO: make this only for current user 
