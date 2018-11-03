7d0487e0e8d7f3f5b1a69d37b11d88bb5da363e4
Adam Demirel <adxm@msn.com>
2018-11-03 19:44:48 +1100

    Add edit and destroy links to service reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -56,7 +56,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_back fallback_location: root_path, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
