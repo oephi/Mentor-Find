07b4d072365c6e22d9500437f29a34d50b46add7
Adam Demirel <adxm@msn.com>
2018-11-05 16:49:37 +1100

    Add template for overview profiles

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,7 +6,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :aws
   
-  resize_to_fill(250, 250)
+  resize_to_fill(250, 286)
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
 
   devise_for :users 
 
-  get '/profile', to: 'feeds#show', as: 'profile' 
+  resources :profiles, except: [:index]
+  get '/profile', to: 'profiles#index'
+  #get '/profile/:id', to: 'profiles#show'
+
+
   get '/purchase_history', to: 'purchases#index'
 
   root to: "feeds#home" # For Devise to work, ensure you have defined root_url to *something*.
