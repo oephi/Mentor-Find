44de14e9e57576a78545863e113666e91f43232a
Adam Demirel <adxm@msn.com>
2018-11-05 14:34:49 +1100

    Give User a default avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,7 +6,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :aws
   
-  resize_to_fill(200, 200)
+  resize_to_fill(250, 250)
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -15,12 +15,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("default-profile.png")
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -37,7 +34,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png bmp)
   end
 
   # Override the filename of the uploaded files:
