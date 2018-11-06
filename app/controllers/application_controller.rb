f1b530634ab4a0ed27d48b2df5829fdfe82a2804
oephi <r.sickle@protonmail.com>
2018-11-06 15:25:24 +1100

    phone number sanitized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :phonenumber, :avatar, :description])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :city, :phonenumber, :avatar, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :phone, :avatar, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :city, :phone, :avatar, :description])
   end
 end
