fd1149ca188031d506b108233edaf76aeaa7393f
Adam Demirel <adxm@msn.com>
2018-11-08 19:08:54 +1100

    titleize name callback during signup

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -15,9 +15,13 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  before_create :titleize
+  def titleize
+    self.name = self.name.titleize
+  end
+
   # Creates stripe customer_id at registration 
   after_create_commit :add_customer_id
-  
   def add_customer_id
     if self.customer_id.nil?
       customer = Stripe::Customer.create(
