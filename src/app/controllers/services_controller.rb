20a1174ac952bea3146794625cc48a4b1b8d28ec
oephi <r.sickle@protonmail.com>
2018-11-08 17:18:58 +1100

    successful service redirect changed

--- src/app/controllers/services_controller.rb
+++ src/app/controllers/services_controller.rb
@@ -39,7 +39,7 @@ def show
     
     respond_to do |format|
       if @service.save
-        format.html { redirect_to root_path, notice: 'Service was successfully created.' }
+        format.html { redirect_to @service, notice: 'Service was successfully created.' }
       else
         format.html { render :new }
       end
